4a5fb80a4a1f4df05d433df31b5072cb99bdce26
macbeamish <macbeamish@gmail.com>
2023-02-23 22:27:30 +1300

    events and collisions

--- game.js
+++ game.js
@@ -1,30 +1,54 @@
-// function moveUp(){
-//     var skeleton = document.querySelectorAll('#skeleton');
-//     skeleton.setAttribute('position', '0 7 -5');
-// }
-// var scene = document.querySelector('a-scene');
-// if (scene.hasLoaded) {
-//     moveUp();
-// } else {
-//     scene.addEventListener('loaded', moveUp);
-// }
-
-// AFRAME.registerComponent("jump",{
-//     init: function(){
-//         var el = this.el;
-//         el.addEventListener('click', function(){
-//             var newPos = "0 7 -5"
-//             el.setAttribute('position', newPos);
-//         });
-//     }
-// });
-
+console.log("game.js loaded");
+AFRAME.registerComponent("sprite-jump", {
+    init: function() {
+      // when clicked attach the body and the shape, and apply the impulse
+      this.el.addEventListener("keydown", evt => {
+        if(keyCode === 32 ){
+        const force = new Ammo.btVector3(0, 2, 0);
+        const pos = new Ammo.btVector3(skeleton.object3D.position.x, skeleton.object3D.position.y, skeleton.object3D.position.z);
+        ball.body.applyImpulse(force, pos);
+        Ammo.destroy(force);
+        Ammo.destroy(pos);}
+        else{console.log("wrong key")}
+      })
+      // check if the events are working by changing a the boxes color
+      document.querySelector("#backboard").addEventListener("collidestart", evt => {
+        document.querySelector("#backboard").setAttribute("color", "green");
+      })
+    }
+  })
 AFRAME.registerComponent("random-hex", {
     init: function(){
-        var el = this.el;
-        el.addEventListener('click', function(){
-            var hex = "#" + Math.random().toString(16).substring(-6);
-            el.setAttribute('material', 'color', hex);
+      this.el.addEventListener("click", evt => {
+        console.log("box clicked")
+        this.el.setAttribute("material", {
+          color: "white"
+        });
         });
     }
 });
+
+  AFRAME.registerComponent("foo", {
+    init: function() {
+      // when clicked attach the body and the shape, and apply the impulse
+      this.el.addEventListener("click", evt => {
+        this.el.setAttribute("ammo-body", {
+          type: "dynamic"
+        });
+        this.el.setAttribute("ammo-shape", {
+          type: "sphere",
+          fit: "manual",
+          sphereRadius: 0.15
+        });
+        const force = new Ammo.btVector3(0, 7, -3);
+        const pos = new Ammo.btVector3(this.el.object3D.position.x, ball.object3D.position.y, ball.object3D.position.z);
+        ball.body.applyImpulse(force, pos);
+        Ammo.destroy(force);
+        Ammo.destroy(pos);
+      })
+      // check if the events are working by changing a the boxes color
+      document.querySelector("#backboard").addEventListener("collidestart", evt => {
+        document.querySelector("#backboard").setAttribute("color", "green");
+      })
+    }
+  })
